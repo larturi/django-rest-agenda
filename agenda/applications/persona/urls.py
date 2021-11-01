ef4b160ba136a1301ee51fd1bbc490680242a554
lea.arturi <lea.arturi@gmail.com>
2021-11-01 10:01:40 -0300

    PersonsSearchListApiView

--- agenda/applications/persona/urls.py
+++ agenda/applications/persona/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 
-from .views import PersonsListApiView
+from .views import PersonsListApiView, PersonsSearchListApiView
 
 app_name = 'person_app'
 
 urlpatterns = [
     path('api/person/list/', PersonsListApiView.as_view(), name='persons'),
+    path('api/person/search/<kword>/', PersonsSearchListApiView.as_view()),
 ]
--- agenda/applications/persona/views.py
+++ agenda/applications/persona/views.py
@@ -10,3 +10,12 @@ class PersonsListApiView(ListAPIView):
 
     def get_queryset(self):
         return Person.objects.all()
+
+class PersonsSearchListApiView(ListAPIView):
+    serializer_class = PersonSerializer
+
+    def get_queryset(self):
+        kword = self.kwargs['kword']
+        return Person.objects.filter(
+            full_name__icontains=kword
+        )
