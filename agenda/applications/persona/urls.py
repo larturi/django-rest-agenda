6b2dd58ab2fc0dc5a3333d404d48f76252b786a0
lea.arturi <lea.arturi@gmail.com>
2021-11-01 10:14:43 -0300

    Create and Detail

--- agenda/applications/persona/urls.py
+++ agenda/applications/persona/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 
-from .views import PersonsListApiView, PersonsSearchListApiView
+from .views import PersonCreateApiView, PersonDetailApiView, PersonsListApiView, PersonsSearchListApiView
 
 app_name = 'person_app'
 
 urlpatterns = [
-    path('api/person/list/', PersonsListApiView.as_view(), name='persons'),
+    path('api/person/list/', PersonsListApiView.as_view()),
     path('api/person/search/<kword>/', PersonsSearchListApiView.as_view()),
+    path('api/person/create/', PersonCreateApiView.as_view()),
+    path('api/person/detail/<pk>/', PersonDetailApiView.as_view()),
 ]
--- agenda/applications/persona/views.py
+++ agenda/applications/persona/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveAPIView
+)
 
 from .models import Person
 from .serializers import PersonSerializer
@@ -19,3 +23,10 @@ class PersonsSearchListApiView(ListAPIView):
         return Person.objects.filter(
             full_name__icontains=kword
         )
+class PersonCreateApiView(CreateAPIView):
+    serializer_class = PersonSerializer
+
+class PersonDetailApiView(RetrieveAPIView):
+    serializer_class = PersonSerializer
+
+    queryset = Person.objects.all()
